945af4e552b74d5078ff024589e84f8388e0ae38
Matheus Miranda <miranda.mhs0@gmail.com>
2024-04-14 19:20:25 -0300

    feat(#1): tables created

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('password');
+            $table->string('middle_name')->nullable();
+            $table->text('bio')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
