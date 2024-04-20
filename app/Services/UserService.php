fdebe1351f95c42ae86e9d2f58c9ebcffe86fbc6
Matheus Miranda <miranda.mhs0@gmail.com>
2024-04-19 23:51:20 -0300

    feat(#7): topics api implemented - close #7

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,8 +3,40 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class UserService extends AbstractService
 {
 	protected $model = User::class;
+
+	public function index(\Illuminate\Http\Request $properties)
+	{
+		$payload = QueryBuilder::for($this->model)
+			->allowedFilters([
+				'id',
+				'name',
+				'surname',
+				'email',
+				'username',
+			])
+			->allowedSorts([
+				'updated_at',
+				'name',
+			])
+			->defaultSort('-updated_at')
+			->paginate($properties['per_page'])
+			->appends($properties->query());
+
+		return $payload;
+	}
+
+	public function show(int|Model $resource): Model|null
+	{
+		return $resource->loadMissing([
+			'articles' => fn (Builder $query) => $query->orderByDesc('updated_at')->limit(5),
+			'topics' => fn (Builder $query) => $query->orderByDesc('updated_at')->limit(5),
+		]);
+	}
 }
