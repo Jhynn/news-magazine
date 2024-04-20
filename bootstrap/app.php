0ae6375a0c666a2c06c70bdf649ed9a6d39b8295
Matheus Miranda <miranda.mhs0@gmail.com>
2024-04-20 00:24:53 -0300

    feat(#6): global middlewares updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Middleware\{
+    ActiveUsers,
+    Cors,
+    CurrentLocation
+};
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,8 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+        $middleware->append([
+            ActiveUsers::class,
+            Cors::class,
+            CurrentLocation::class,
+        ])
+    )
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
